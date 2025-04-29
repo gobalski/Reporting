6739336fddae19e3682c2784d3de8009b32b79b8
root <root@localhost.org>
2025-04-29 13:07:11 +0200

    init

---
+++ render.py
@@ -0,0 +1,82 @@
+import re
+import yaml
+from jinja2 import Template
+import sys,os
+from tempfile import TemporaryDirectory
+import shutil
+
+def load_md_file(file_path):
+    """ Load a Markdown file and return its content and properties. """
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Assuming the properties are in YAML format at the beginning of the file
+    properties_match = re.search(r'---\s*([\s\S]*?)\s*---', content)
+    properties = None
+    
+    if properties_match:
+        properties_str = properties_match.group(1)
+        properties = yaml.safe_load(properties_str)
+    if properties is None:
+        properties = {}
+
+    return content, properties
+
+def render_markdown_with_properties(file_path):
+    """ Load, parse properties and render Markdown file using Jinja2. """
+    content, properties = load_md_file(file_path)
+    
+    # Create a Jinja2 Template
+    template = Template(content)
+    
+    # Render the Markdown content by replacing keys with their values
+    rendered_content = template.render(properties)
+
+    return rendered_content
+
+def render_report(report_path, working_path):
+    for file in os.listdir(report_path):
+        print(" [i] rendering " + file)
+        if file[-2:] != 'md':
+            continue
+        file_path = os.path.join(report_path, file)
+        content = render_markdown_with_properties(file_path)
+
+        out_path = os.path.join(working_path, file)
+        with open(out_path, 'w') as f:
+            f.write(content)
+    
+    os.makedirs(working_path + "/Findings")
+    for file in os.listdir(report_path + "/Findings"):
+        print(" [i] rendering " + file)
+        if file[-2:] != 'md':
+            continue
+        file_path = os.path.join(report_path + "/Findings", file)
+        content = render_markdown_with_properties(file_path)
+
+        out_path = os.path.join(working_path + "/Findings", file)
+        with open(out_path, 'w') as f:
+            f.write(content)
+
+def compile_pdf(working_path):
+    pass
+
+def compile_html(working_path, output_path, filter_path):
+    print("compiling")
+    cmd = "pandoc --verbose -o " + output_path  + "/report.html --highlight-style=tango --lua-filter=" + filter_path +  "/include-files.lua " + working_path +  "/000_Report.md"
+    print(cmd)
+    os.chdir(working_path)
+    os.system(cmd)
+
+    
+
+# Example usage:
+if __name__ == "__main__":
+    report_path = sys.argv[1]
+    report_path = os.path.abspath(report_path)
+    filter_path = os.path.join(os.getcwd(), "pandoc_filters")
+    with TemporaryDirectory() as tmp:
+        render_report(report_path, tmp)
+        shutil.copytree(os.path.join(report_path, "Res"), os.path.join(tmp, "Res"))
+        compile_html(tmp, report_path, filter_path)
+        print("compiling done")
